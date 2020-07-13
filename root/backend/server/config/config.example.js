164fb452992c279865caf88d8144da6807e628bd
mrodan <rodanm@ufl.edu>
2020-07-13 16:37:46 -0400

    Backend structure and database connection

--- root/backend/server/config/config.example.js
+++
@@ -1,9 +0,0 @@
-export default {
-    db: {
-      uri: 'mongodb+srv://dbAdmin:dbAdminPass@cluster0.p3och.mongodb.net/<dbname>?retryWrites=true&w=majority'
-    },
-    jwt: {
-      secretKey: "CEN3031" // This key should be secret 
-    },
-    port: 5000
-  };
