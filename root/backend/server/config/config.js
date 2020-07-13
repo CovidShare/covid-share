511cd492a87d716c4a0d31262e4b1dd38bed6d66
mrodan <rodanm@ufl.edu>
2020-07-13 16:06:50 -0400

    Backend structure and database connection

---
+++ root/backend/server/config/config.js
@@ -0,0 +1,9 @@
+export default {
+    db: {
+      uri: 'mongodb+srv://dbAdmin:dbAdminPass@cluster0.p3och.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    },
+    jwt: {
+      secretKey: "CEN3031" // This key should be secret 
+    },
+    port: 5000
+  };
---
+++ root/backend/server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser';
+import config from './config/config.js';
+
+// DATABASE CONNECTION
+mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
+    console.log("Successfully connected to DB");
+});
+// Event triggers when the connection gives an error
+mongoose.connection.on('error', () => {
+    console.log("Error in connection to DB");
+});
+// Event triggers when the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+    console.log("Connection to DB is closed");
+});
+
+const app = express(); // Init express app
+app.use(morgan('dev')); // Request log
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(config.port, () => {
+    console.log(`App now listening on port ${config.port}`);
+});
